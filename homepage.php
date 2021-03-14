b4bb1e24ae47050d5da08a672c7670d24eb67b25
Daniel Collins <43524556+dpcuva@users.noreply.github.com>
2021-03-13 19:13:44 -0500

    Renamed files and moved nav-bar into its own file

---
+++ homepage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 
+  Bootstrap is designed to be responsive to mobile.
+  Mobile-first styles are part of the core framework.
+   
+  width=device-width sets the width of the page to follow the screen-width
+  initial-scale=1 sets the initial zoom level when the page is first loaded   
+  -->
+
+  <meta name="author" content="Daniel Collins & Grady Roberts">
+  <meta name="description" content="Course Project">
+
+  <title>UVA Jeopardy</title>
+
+  <!--Reset CSS before loading bootstrap-->
+  <link rel="stylesheet" href="styles/reset.css" />
+
+  <!-- 3. link bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+  <!-- 3. Style Sheet for NavBar -->
+  <link rel="stylesheet" href="styles/navbar.css" />
+</head>
+
+<body>
+  <?php include("header.html")?>
+  <h3>Hello World!</h3>
+</body>
