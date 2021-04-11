b105bf26d4677fd100639b1781a3255ce903af98
Grady Roberts <gnr7aj@virginia.edu>
2021-04-11 19:11:20 -0400

    db connection test

--- src/loginpage.php
+++ src/loginpage.php
@@ -66,6 +66,33 @@
             }
         }
     </script>
+
+
+    <?php 
+        use PDO;
+        use PDOException;
+
+        $dbUser = getenv('CLOUDSQL_USER');
+        $dbPass = getenv('CLOUDSQL_PASSWORD');
+        $dbName = getenv('CLOUDSQL_DB');
+        $dbConn = getenv('CLOUDSQL_CONN_NAME');
+
+        $dsn = "mysql:unix_socket=/cloudsql/${dbConn};dbname=${dbName}";
+        try {
+            $pdo = new PDO($dsn, $dbUser, $dbPass);
+        } catch (PDOException $e) {
+            echo "Can't connect to db<br/>";
+            echo $e->getMessage() . "<br/>";
+        }
+
+        echo "Connected to db<br/>";
+
+        $statement = $pdo->prepare('SELECT * FROM Users');
+        $statement->execute();
+        $result = $statement->fetch();
+        echo $result . '<br/>';
+    ?>
+
 </body>
 
 </html>
