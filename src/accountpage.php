a1f0303e9f48e681b48e063524f84412c129310a
Daniel Collins <43524556+dpcuva@users.noreply.github.com>
2021-04-13 01:51:42 -0400

    Added Ability to Update Account

--- src/accountpage.php
+++ src/accountpage.php
@@ -11,10 +11,27 @@ global $pdo;
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
   if ($_POST['form'] == "AccountForm") { //Change to the Account
     if ($_POST['button'] == "Update Account") {
-      $query = "SELECT * FROM Users WHERE email = :email AND password = :password";
+      $query = "UPDATE Users SET fname = :fname, lname = :lname WHERE email = :email";
       $statement = $pdo->prepare($query);
-      $statement->bindValue(':email', $_POST['loginemail'], PDO::PARAM_STR);
-      $statement->bindValue(':password', $_POST['loginpwd'], PDO::PARAM_STR);
+      $statement->bindValue(':email', "daniel.p.collins@me.com", PDO::PARAM_STR);
+      if ($_POST['fname'] != "") {
+        $statement->bindValue(':fname', $_POST['fname'], PDO::PARAM_STR);
+        $_SESSION['fname'] = $_POST['fname']
+      } else {
+        $statement->bindValue(':fname', $_SESSION['fname'], PDO::PARAM_STR);
+      }
+      if ($_POST['lname'] != "") {
+        $statement->bindValue(':lname', $_POST['lname'], PDO::PARAM_STR);        
+        $_SESSION['lname'] = $_POST['lname']
+      } else {
+        $statement->bindValue(':lname', $_SESSION['lname'], PDO::PARAM_STR);
+      }
+      //if ($_POST['pwd'] != "") {
+        //$statement->bindValue(':pwd', $_POST['pwd'], PDO::PARAM_STR);
+        //$_SESSION['pwd'] = $_POST['pwd']
+      //} else {
+        //$statement->bindValue(':pwd', $_SESSION['pwd'], PDO::PARAM_STR);
+      //}
       $statement->execute();
       $result = $statement->fetch();
       $statement->closeCursor();
@@ -84,16 +101,16 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
               <input type="text" placeholder="stuff@stuff.com" readonly="readonly" />
             </div>
             <div class="block">
+              <label><b>Password:</b></label>
+              <input type="text" name="pwd" readonly="readonly"/>
+            </div>
+            <div class="block">
               <label><b>First Name:</b></label>
-              <input type="text" />
+              <input type="text" name="fname" />
             </div>
             <div class=" block">
               <label><b>Last Name:</b></label>
-              <input type="text" />
-            </div>
-            <div class="block">
-              <label><b>Password:</b></label>
-              <input type="text" />
+              <input type="text" name="lname" />
             </div>
           </form>
           <input type="hidden" name="form" value="AccountForm" />
