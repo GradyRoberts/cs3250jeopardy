0d28c3f2257b6f96ac10cd49d1b200e7d8b34f63
Grady Roberts <gnr7aj@virginia.edu>
2021-04-13 09:13:38 -0400

    unset cookies on logout

--- src/logout.php
+++ src/logout.php
@@ -1,5 +1,18 @@
 <?php
 function logout() {
+    if (count($_COOKIE) > 0)
+    {
+        foreach ($_COOKIE as $key => $value)
+        {
+            // Deletes the variable (array element) where the value is stored in this PHP.
+            // However, the original cookie still remains intact in the browser.   	
+            unset($_COOKIE[$key]);   
+		
+            // To completely remove cookies from the client, 
+            // set the expiration time to be in the past
+            setcookie($key, '', time() - 3600);
+        }
+    }
     session_destroy();
     header("Location: https://cs3250-jeopardy.uk.r.appspot.com/loginpage.php");
 }
